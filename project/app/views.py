c2b52803774dfd888cb6ecce6bfad0aec43196af
kipteamm <toro.een@gmail.com>
2025-07-31 17:07:01 +0200

    Added fonts Started on diagram creation process

--- project/app/views.py
+++ project/app/views.py
@@ -7,3 +7,13 @@ app_blueprint = Blueprint("app", __name__)
 @app_blueprint.get("/")
 def index():
     return render_template("app/index.html")
+
+
+@app_blueprint.get("/app")
+def app():
+    return render_template("app/app.html")
+
+
+@app_blueprint.get("/app/create")
+def create():
+    return render_template("app/new_project.html")
