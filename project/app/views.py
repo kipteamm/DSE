ad935490104bdb758d60c54b15469615d3691d98
kipteamm <toro.een@gmail.com>
2025-08-05 17:17:01 +0200

    Fixed issues with email authentication

--- project/app/views.py
+++ project/app/views.py
@@ -9,8 +9,8 @@ app_blueprint = Blueprint("app", __name__)
 
 @app_blueprint.get("/test")
 def test():
-    return render_template("email/verification.html", user="toro.een", code="123456", app_domain=request.host_url, email="toro.een@gmail.com")
-    # return render_template("auth/awaiting_verification.html", sender="AAA@gmail.com", email="toro.een@gmail.com", next="/app")
+    # return render_template("email/verification.html", user="toro.een", code="123456", app_domain=request.host_url, email="toro.een@gmail.com")
+    return render_template("auth/awaiting_verification.html", sender="AAA@gmail.com", email="toro.een@gmail.com", next="/app")
 
 
 @app_blueprint.get("/app")
